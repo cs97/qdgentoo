4f6e67e35764fc020d671a4f7cbafaabd5386a67
leftside97 <l3f7s1d3@protonmail.com>
2021-01-09 01:26:54 +0100

    Update qdgentoo-efi.sh

--- qdgentoo-efi.sh
+++ qdgentoo-efi.sh
@@ -47,6 +47,12 @@ banner(){
 ################################	0
 makefs(){
 	#(echo o; echo n; echo p; echo 1; echo ""; echo +64M; echo n; echo p; echo 2; echo ""; echo ""; echo w; echo q) | fdisk /dev/$disk
+	#parted /dev/$disk --script mklabel gpt
+	#parted /dev/$disk --script mkpart primary ext4 32MiB 100MiB
+	#parted /dev/$disk --script mkpart primary ext4 100MiB 1GiB
+	#parted /dev/$disk --script mkpart primary ext4 1GiB 30GiB
+	#parted /dev/$disk --script mkpart primary ext4 30GiB 100%
+
 	cfdisk $disk
 	sleep 1
 	mkfs.fat -F 32 $boot
