790edc4d7e41cb74d8823e9750659b1cd3771f47
leftside97 <l3f7s1d3@protonmail.com>
2021-01-12 12:30:44 +0100

    Update qdgentoo-i3.sh

--- qdgentoo-i3.sh
+++ qdgentoo-i3.sh
@@ -14,7 +14,6 @@ banner(){
 	echo "#  0  add user                           #"
 	echo "#  1  xorg-server                        #"
 	echo "#  2  i3                                 #"
-	echo "#  3  firefox                            #"
 	echo "#  99 update                             #"
 	echo "##########################################"
 	echo ""
@@ -35,19 +34,17 @@ case $1 in
 		usermod -a -G input $USER;;
 	"1") 
 		USE="-suid" emerge --ask x11-base/xorg-server
-		echo 'SUBSYSTEM=="input", ACTION=="add", GROUP="input"' > /etc/udev/rules.d/99-dev-input-group.rules
+		#echo 'SUBSYSTEM=="input", ACTION=="add", GROUP="input"' > /etc/udev/rules.d/99-dev-input-group.rules
 		emerge --ask pulseaudio alsa-mixer alsa-utils
 		#emerge --ask x11-base/xorg-server --autounmask-write; source /etc/profile
 		#emerge --ask x11-base/xorg-server; source /etc/profile
 		;;
 	"2")
-		emerge --ask x11-wm/i3 x11-misc/i3status x11-misc/i3lock x11-terms/xterm edia-gfx/feh sys-process/htop
+		emerge --ask x11-wm/i3 x11-misc/i3status x11-misc/i3lock x11-terms/xterm sys-process/htop #edia-gfx/feh
 		echo "exec i3" > ~/.xinitrc
 		mv ~/.config/i3/config ~/.config/i3/config.old
 		wget https://raw.githubusercontent.com/leftside97/qdgentoo/master/config
 		mv ~/config ~/.config/i3/config;;
-	"3")
-		emerge --ask www-client/firefox;;
 	"99")
 		mv qdgentoo-i3.sh qdgentoo-i3.old
 		wget https://raw.githubusercontent.com/leftside97/qdgentoo/master/qdgentoo-i3.sh
