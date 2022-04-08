e5d80855dcc324931ab4f15b529833efd4e67eb7
leftside97 <chrswr97@gmail.com>
2022-04-08 17:52:52 +0200

    Update qdgentoo.sh

--- qdgentoo.sh
+++ qdgentoo.sh
@@ -270,6 +270,12 @@ install_sway(){
 	USE="wallpapers" emerge --ask gui-wm/sway
 }
 ################################	14
+sway_config(){
+	mv ~/.config/i3/config ~/.config/i3/config.old
+	wget https://raw.githubusercontent.com/leftside97/qdgentoo/master/conf/config
+	mv ~/config ~/.config/i3/config
+}
+################################	15
 mount_again(){
 	mkdir /mnt/gentoo
 	sleep 1
@@ -289,7 +295,7 @@ mount_again(){
 	sleep 1
 	chroot /mnt/gentoo /bin/bash
 }
-################################	15
+################################	16
 install_wifi(){
 	emerge --ask networkmanager
 	emerge --ask nm-applet
@@ -314,8 +320,9 @@ case $1 in
 	"11") add_user;;
 	"12") install_wayland;;
 	"13") install_sway;;
-	"14") mount_again;;
-	"15") install_wifi;;	
+	"14") sway_config;;
+	"15") mount_again;;
+	"16") install_wifi;;	
 	"99")
 		mv qdgentoo.sh qdgentoo.old
 		wget https://raw.githubusercontent.com/leftside97/qdgentoo/master/qdgentoo.sh
