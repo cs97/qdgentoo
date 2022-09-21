220bb8c9c906e2f396ba5dd050a76fe19a31f5a5
cs97 <chrswr97@gmail.com>
2022-09-21 17:49:38 +0200

    .

--- runsway.sh
+++ runsway.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
-# start sway on OpenRC and Systemd
-
-# quick and dirty fix for OpenRC
-
-#[ -d /run/systemd/system ] || 
-    
-
-[ -z ${XDG_RUNTIME_DIR} ] && {
-     export XDG_RUNTIME_DIR=/tmp/${UID}-runtime-dir
-    [ -d ${XDG_RUNTIME_DIR} ] && {
-        mkdir ${XDG_RUNTIME_DIR}
-        chmod 0700 ${XDG_RUNTIME_DIR}
-    }
-}
-
 
 case $1 in
 	"--igpu") [ -d /dev/dri/card1 ] && WLR_DRM_DEVICES="/dev/dri/card1" sway --unsupported-gpu || WLR_DRM_DEVICES="/dev/dri/card0" sway --unsupported-gpu;;
