4cd2948ff573b83f7cd74ded1dfa6f5d8a1521e9
cs97 <chrswr97@gmail.com>
2022-11-20 03:22:44 +0100

    Update install-virtualbox.sh

--- shell-script/install-virtualbox.sh
+++ shell-script/install-virtualbox.sh
@@ -4,6 +4,7 @@ cat <<EOF >> /etc/portage/package.use/vbox
 app-emulation/virtualbox X egl
 dev-qt/qtgui X egl
 x11-libs/libxkbcommon X
+media-libs/libsdl X
 EOF
 
 emerge --ask --verbose app-emulation/virtualbox
