58c9b4497e5387007d01890e9efb39ae9452367f
cs97 <chrswr97@gmail.com>
2023-01-21 22:33:23 +0100

    much wow

--- qdgentoo.sh
+++ qdgentoo.sh
@@ -71,7 +71,7 @@ simple_banner(){
 	echo -e "\tinstall \t\tinstall base system"
 	echo -e "\tfirst_boot \t\trun on first boot"
 	echo -e "\tadd_user \t\tadd user"
-	echo -e "\tinstall_desktop \tinstall sway desktop"
+	echo -e "\tinstall_sway \tinstall sway desktop"
 	echo -e "\tinstall_wifi"
 	echo -e "\tinstall_audio"
 	echo -e "\tupdate \t\t\tupdate installer"
