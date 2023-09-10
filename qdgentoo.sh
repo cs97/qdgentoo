12f85f80cdeb63552983c1b011afdc79cdf13242
cs97 <chrswr97@gmail.com>
2023-09-10 13:21:10 +0200

    much wow

--- qdgentoo.sh
+++ qdgentoo.sh
@@ -22,7 +22,7 @@ aes_yesno=false
 # false=automode true=cfdsik
 use_cfdisk=false
 
-simple_mode=true
+simple_mode=false
 
 #install_virtualbox-guest-additions
 vbox=false
@@ -191,10 +191,6 @@ makefs_2(){
 	mount --make-rslave /mnt/gentoo/dev
 	sleep 1
 	
-	if [ $simple_mode = false ]; then 
- 		cp qdgentoo.sh /mnt/gentoo/root/qdgentoo.sh
- 		chroot /mnt/gentoo /bin/bash
-   	fi
 }
 ################################	1
 do_in_chroot(){
@@ -205,13 +201,11 @@ do_in_chroot(){
 	emerge --sync
 	clear
 	eselect profile list
-}
-################################	2
-at_world(){
+
+
 	emerge --verbose --update --deep --newuse @world			
-}
-################################	3
-make_locale(){
+
+
 #	portageq envvar ACCEPT_LICENSE @FREE
 	#echo "Europe/Berlin" > /etc/timezone
 	echo $timezone > /etc/timezone	
@@ -233,27 +227,23 @@ make_locale(){
 	clear
 	eselect locale set 6
 	eselect locale list
-	}
-################################	4
-env_update(){
+
+
 	env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
 	echo -e "\n### etc-update ###\n"
 	etc-update
-}
-################################	5
-gentoo_sources(){
+
+
 	echo "$kernel" > /etc/portage/package.accept_keywords/kernel
 	echo "sys-kernel/gentoo-sources experimental" >> /etc/portage/package.use/kernel
 	emerge sys-kernel/gentoo-sources
 	etc-update
-}
-################################	6
-pci_utils(){
+
+
 	emerge sys-apps/pciutils
 	etc-update
-}
-################################	7
-gentoo_genkernel(){
+
+
 	mkdir /etc/portage/package.license
 	echo "sys-kernel/linux-firmware @BINARY-REDISTRIBUTABLE" > /etc/portage/package.license/firmware
 	emerge genkernel
@@ -277,9 +267,8 @@ gentoo_genkernel(){
 	  	fi
     
 	fi
-}
-################################	8
-fstab_stuff(){
+
+
 	if [ $aes_yesno = true ]; then
 		echo "/dev/mapper/vg0-root		/		ext4		defaults	0 0" >> /etc/fstab
 		echo "$boot		/boot		vfat		defaults        0 0" >> /etc/fstab
@@ -306,9 +295,6 @@ fstab_stuff(){
 	passwd
 
 
-}
-################################	9.2
-install_grub_efi(){
 	if [ -d /sys/firmware/efi ]; then
 		echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
 	fi
@@ -407,17 +393,7 @@ install_base_system(){
 }
 do_1_to_9(){
 	do_in_chroot
-	at_world
-	make_locale
-	env_update
-	gentoo_sources
-	pci_utils
-	gentoo_genkernel
-	fstab_stuff
-	install_grub_efi
- 	if $vbox; then
-		install_virtualbox-guest-additions
-  	fi
+
    	echo "installation complete!"
    	echo "exit for reboot"
    	bash
@@ -489,27 +465,10 @@ install_nvidia(){
 	echo "x11-drivers/nvidia-drivers -tools" >> /etc/portage/package.use/nvidia
 	emerge --ask x11-drivers/nvidia-drivers
 }
-################################	20
-install_tools(){
-	#echo "xfce-base/thunar udisks" > /etc/portage/package.use/thunar
-	emerge --ask xfce-base/thunar app-arch/file-roller
-	emerge --ask app-editors/vim
-	emerge --ask sys-process/htop
-	emerge --ask app-misc/neofetch
-	emerge --ask dev-lang/rust
-	emerge --ask dev-vcs/git
-	emerge --ask sys-apps/lm-sensors
-	#emerge --ask sys-power/cpupower
 
-	#gut
-	#wget https://raw.githubusercontent.com/cs97/qdgentoo/master/shell-script/gentoo-update-tool.sh
-	#mv gentoo-update-tool.sh /usr/bin/gut
-	#chmod +x /usr/bin/gut
-
-}
 ################################	21
 my_config(){
-	emerge --ask x11-terms/alacritty
+	emerge --ask dev-lang/rust dev-vcs/git x11-terms/alacritty
 
 	mkdir .config
 	mkdir .config/sway
@@ -530,24 +489,11 @@ my_config(){
 	mv .bashrc .bashrc.old
 	wget https://raw.githubusercontent.com/cs97/qdgentoo/master/conf/.bashrc
 }
-################################	install virtualbox-guest-additions
-install_virtualbox-guest-additions(){
-	emerge app-emulation/virtualbox-guest-additions
 
-	if [ -d /run/systemd/system ]; then
-
-		systemctl enable --now virtualbox-guest-additions
-	else
-		rc-update add virtualbox-guest-additions default
-		rc-update add dbus default
-	fi
-
-	gpasswd -a $USER vboxguest
-}
 ################################	99
 update_installer(){
 	mv qdgentoo.sh qdgentoo.old
-	wget https://raw.githubusercontent.com/cs97/qdgentoo/master/qdgentoo.sh
+	wget https://raw.githubusercontent.com/cs97/qdgentoo/simplified-2.x/qdgentoo.sh
 	chmod +x qdgentoo.sh
 	chmod -x qdgentoo.old
 }
@@ -558,47 +504,19 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 
-if [ $simple_mode = true ]; then
-	case $1 in
-		"install") install_base_system;;	#base system install
-		"first_boot") first_boot;;
-		"add_user") add_user;;
-		"install_sway") install_wayland_sway;;
-		"install_wifi") install_wifi;;
-		"install_audio") install_audio;;
-		"install_virtualbox-guest-additions") install_virtualbox-guest-additions;;
+case $1 in
+	"install") install_base_system;;	#base system install
+	"first_boot") first_boot;;
+	"add_user") add_user;;
+	"install_sway") install_wayland_sway;;
+	"install_wifi") install_wifi;;
+	"install_audio") install_audio;;
+	"install_virtualbox-guest-additions") install_virtualbox-guest-additions;;
 
-		"1to9") do_1_to_9;;
-		"update") update_installer;;
-		*) simple_banner;;
-	esac
-else
-	case $1 in
-		"0") [ $aes_yesno = true ] && makefs_aes || makefs;;
-		"1") do_in_chroot;;
-		"2") at_world;;
-		"3") make_locale;;
-		"4") env_update;;
-		"5") gentoo_sources;;
-		"6") pci_utils;;
-		"7") gentoo_genkernel;;
-		"8") fstab_stuff;;
-		"9") install_grub_efi;;
-		"10") umount_all;;
-		"11") mount_again;;
-		"12") first_boot;;
+	"1to9") do_1_to_9;;
+	"update") update_installer;;
+	*) simple_banner;;
+esac
 
-		"14") add_user;;
-		"15") install_wayland_sway;;
-		"16") install_audio;;
-		"17") install_wifi;;
-		"18") install_amdgpu;;
-		"19") install_nvidia;;
-		"20") install_tools;;
-		"21") my_config;;
-		"99") update_installer;;
-		*) banner;;
-	esac
-fi
 
 exit
