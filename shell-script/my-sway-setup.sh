7a977332bfceeca65283a010b76c06681c71ecc9
cs97 <chrswr97@gmail.com>
2023-09-15 15:23:22 +0200

    Create my-sway-setup.sh

---
+++ shell-script/my-sway-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+emerge --ask dev-lang/rust dev-vcs/git x11-terms/alacritty
+
+mkdir .config
+mkdir .config/sway
+mv .config/sway/config .config/sway/config.old
+
+#sway status
+git clone https://github.com/cs97/rusty-sway-status
+cd rusty-sway-status
+cargo build --release
+cp target/release/status /usr/bin/status
+cd ..
+
+#sway .config
+wget https://raw.githubusercontent.com/cs97/qdgentoo/master/conf/sway-config
+mv sway-config .config/sway/config
+
+#.bashrc
+mv .bashrc .bashrc.old
+wget https://raw.githubusercontent.com/cs97/qdgentoo/master/conf/.bashrc
