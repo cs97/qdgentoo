f9cd13277edc706f64d37050be60df2b8d0e5e15
cs97 <chrswr97@gmail.com>
2023-10-02 22:41:51 +0200

    Update install-qemu.sh

--- shell-script/install-qemu.sh
+++ shell-script/install-qemu.sh
@@ -7,4 +7,4 @@ app-emulation/qemu qemu_user_targets_x86_64
 dev-libs/libclc spirv
 EOF
 
-emerge --ask --verbose app-emulation/qemu
+emerge --ask --verbose app-emulation/qemu sys-firmware/edk2-ovmf-bin
