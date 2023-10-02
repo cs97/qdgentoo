084bf392e9218ca99cf5c9de477e742419614c3b
cs97 <chrswr97@gmail.com>
2023-10-02 14:32:49 +0200

    Create install-qemu.sh

---
+++ shell-script/install-qemu.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cat <<EOF >> /etc/portage/package.use/qemu
+app-emulation/qemu gtk sdl opengl virgl spice
+app-emulation/qemu  qemu_softmmu_targets_x86_64
+app-emulation/qemu qemu_user_targets_x86_64
+dev-libs/libclc spirv
+EOF
+
+emerge --ask --verbose app-emulation/qemu
