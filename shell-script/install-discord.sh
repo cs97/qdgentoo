6e5751c2a36fd5161272cd5179af6097f0b01a11
cs97 <chrswr97@gmail.com>
2024-01-09 20:48:07 +0100

    Update install-discord.sh

--- shell-script/install-discord.sh
+++ shell-script/install-discord.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
-echo "net-im/discord-bin all-rights-reserved" > /etc/portage/package.license/discord
+#echo "net-im/discord-bin all-rights-reserved" > /etc/portage/package.license/discord
 
-echo "net-im/discord-bin ~amd64" > /etc/portage/package.accept_keywords/discord
+#echo "net-im/discord-bin ~amd64" > /etc/portage/package.accept_keywords/discord
 
-echo "app-text/ghostscript-gpl cups" > /etc/portage/package.use/discord
+#echo "app-text/ghostscript-gpl cups" > /etc/portage/package.use/discord
 
-emerge --ask net-im/discord-bin media-libs/libpulse
+echo "net-im/discord all-rights-reserved" > /etc/portage/package.license/discord
+
+#emerge --ask net-im/discord-bin media-libs/libpulse
+
+emerge --ask --verbose net-im/discord
